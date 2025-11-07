f0fdd46542091b3ddb3f2d87b719f93c50399b79
Walterb0b <osca0001@stud.ek.dk>
2025-11-07 14:40:02 +0100

    ændret kommentarer

--- src/main/ParkingGate.java
+++ src/main/ParkingGate.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingGate {
-    private List<Car> parkedCars;
+    private final List<Car> parkedCars;
     private GateState state;
     int capacity;
 
